b4e588add402b2c76b0263f3ff2eef95fc899164
matuchiprut <166836106+matuchiprut@users.noreply.github.com>
2024-06-21 16:54:19 -0300

    Update dataset_test.py

--- tp2-v2(1)/tp2/templates/dataset_test.py
+++ tp2-v2(1)/tp2/templates/dataset_test.py
@@ -1,14 +1,20 @@
-import unittest
+mport unittest
 
 # Importamos el codigo a testear.
 from dataset import DataSetCampanasVerdes
+d:DataSetCampanasVerdes = DataSetCampanasVerdes('campanas-verdes-acortado.csv')
 
 ####################################################################
 
 class TestDataSetCampanasVerdes(unittest.TestCase):
 
-    def test_...(self):
-        ...
+    def test_tamano(self):
+        self.assertEqual(d.tamano,6)
+        self.assertNotEqual(d.tamano,-1)
+        self.assertNotEqual(d.tamano,50)
+    
+    def test_barrios(self):
+        self.assertEqual(d.barrios,{"CHACARITA","MONTE CASTRO","NUEVA POMPEYA","VILLA DEVOTO","VILLA CRESPO"})
 
 ## y así con el resto de los métodos a testear.
         
