db698f9128a82437e25d0f7a2ded47748521dbd0
BenjaminChejtman <166836220+BenjaminChejtman@users.noreply.github.com>
2024-06-22 16:36:21 -0300

    Update campana_verde_test.py

--- tp2-v2(1)/tp2/templates/campana_verde_test.py
+++ tp2-v2(1)/tp2/templates/campana_verde_test.py
@@ -7,20 +7,20 @@ from campana_verde import CampanaVerde
 class TestCampanaVerde(unittest.TestCase):
 
     def test_atributos_CampanaVerde(self): #Acá se desea testear que la separación de la lista que debería ingresar y cortar el csv en partes funciona. Utilizé dos ejemplos simples de campanas verdes reales sacadas de Google Maps.         
-        campana1:CampanaVerde=CampanaVerde("Laprida 1149","Recoleta",2,{"Plástico"},(-34.59560785539046, -58.40803234691601))
+        campana1:CampanaVerde=CampanaVerde("Laprida 1149","Recoleta",2,{"Plástico"},(-58.40803234691601, -34.59560785539046))
         self.assertEqual(campana1.direccion,"Laprida 1149")
         self.assertEqual(campana1.barrio,"Recoleta")
         self.assertEqual(campana1.comuna,2)
         self.assertEqual(campana1.materiales,{"Plástico"})
-        self.assertAlmostEqual(campana1.latitudylongitud, (-34.59560785539046, -58.40803234691601))
+        self.assertAlmostEqual(campana1.latitudylongitud, (-58.40803234691601, -34.59560785539046))
         #self.assertAlmostEqual(campana1.longitud, "-58.40803234691601")
         
-        campana2:CampanaVerde=CampanaVerde("Ibarrola 7051","Liniers",9,{"Papel","Cartón"},(-34.64016129701416, -58.525036515246306))
+        campana2:CampanaVerde=CampanaVerde("Ibarrola 7051","Liniers",9,{"Papel","Cartón"},(-58.525036515246306, -34.64016129701416))
         self.assertEqual(campana2.direccion,"Ibarrola 7051")
         self.assertEqual(campana2.barrio,"Liniers")
         self.assertEqual(campana2.comuna,9)
         self.assertEqual(campana2.materiales,{"Papel","Cartón"})
-        self.assertAlmostEqual(campana2.latitudylongitud, (-34.64016129701416, -58.525036515246306))
+        self.assertAlmostEqual(campana2.latitudylongitud, (-58.525036515246306, -34.64016129701416))
         #self.assertEqual(campana2.longitud, "-58.525036515246306")
         
     def test_CampanaVerde_distancia(self): #En este conjunto de tests vamos a revisar si la función en sí es correcta y hace lo que le pedimos.
