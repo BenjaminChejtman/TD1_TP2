e9675be1a642d7d892c3d75cb010ebe0b6d14daf
BenjaminChejtman <166836220+BenjaminChejtman@users.noreply.github.com>
2024-06-23 13:01:38 -0300

    Update dataset.py

--- tp2-v2(1)/tp2/templates/dataset.py
+++ tp2-v2(1)/tp2/templates/dataset.py
@@ -27,7 +27,7 @@ class DataSetCampanasVerdes:
         Requiere: un DataSet D de Campanas Verdes #deberiamos poner nada?
         Devuelve: la cantidad de Campanas Verdes en el DataSet D
         '''
-        return len(self.campanas)
+        return len(self.campanas) #retornamos la len self.campanas
 
     def barrios(self) -> set[str]: 
         '''
@@ -37,8 +37,8 @@ class DataSetCampanasVerdes:
         '''
         vrBarrios:set[str] = set()
         for campana in self.campanas:   #campana es cada campana verde en el CSV representada por lo que devuelve su REPR
-            vrBarrios.add(campana.barrio)   
-        return vrBarrios
+            vrBarrios.add(campana.barrio)   #agregamos a vrBarrios el barrio correspondiente a cada campana, y como en un set no se repite no es necesario checkear si ya estaba o no dicho barrio
+        return vrBarrios #retornamos el set de los barrios
     
     def campanas_del_barrio(self, barrio:str) -> list[CampanaVerde]:
         '''
@@ -72,10 +72,10 @@ class DataSetCampanasVerdes:
         '''
         campanasPorBarrio:dict[str, int] = dict()
         for campana in self.campanas:
-            if material in campana.materiales and campana.barrio in campanasPorBarrio: 
-                campanasPorBarrio[campana.barrio] = campanasPorBarrio[campana.barrio] + 1
-            elif material in campana.materiales and campana.barrio not in campanasPorBarrio:
-                campanasPorBarrio[campana.barrio] = 1  
+            if material in campana.materiales and campana.barrio in campanasPorBarrio: # Nos fijamos si la campana verde permite el material indicado y si el barrio donde esta ya se encuentra en el Dict camapanasPorBarrio
+                campanasPorBarrio[campana.barrio] = campanasPorBarrio[campana.barrio] + 1 # En caso de ser asi le agregamos uno al numero que ya habia
+            elif material in campana.materiales and campana.barrio not in campanasPorBarrio: # En caso de que el material este permitido en esa campana verde pero aun no se encuentre en el Dict...
+                campanasPorBarrio[campana.barrio] = 1  # ... Agregamos el barrio al Dict con un valor inicial de 1
         return campanasPorBarrio
 
     def tres_campanas_cercanas(self, punto:tuple[float, float]) -> tuple[CampanaVerde, CampanaVerde, CampanaVerde]: #la consigna pide lat y lng como parametros por separado, preguntar si ak hacerlo como una tupla como nosotros lo hicimos esteria bien o mal
@@ -83,14 +83,12 @@ class DataSetCampanasVerdes:
         Requiere: Un punto del tipo Tuple que contenga su Latitud y su Longitud, ambas de tipo Float
         Devuelve: una tupla con sus tres objetos del tipo CampanaVerde, que serian las traes Campanas Verdes mas cercanas al punto ingresado
         '''
-        tresMasCercanas:tuple[CampanaVerde, CampanaVerde, CampanaVerde] = (self.campanas[0], self.campanas[1], self.campanas[2])
-        for campana in self.campanas:
-            
+        tresMasCercanas:tuple[CampanaVerde, CampanaVerde, CampanaVerde] = (self.campanas[0], self.campanas[1], self.campanas[2]) #empezamos poniendo las tres primeras campanas que tenemos en el CSV
+        for campana in self.campanas:            
             dist = campana.distancia(punto)
             primero = tresMasCercanas[0].distancia(punto)
             segundo = tresMasCercanas[1].distancia(punto)
-            tercero = tresMasCercanas[2].distancia(punto)
-            
+            tercero = tresMasCercanas[2].distancia(punto)            
             if dist < primero:
                 tresMasCercanas = (campana, tresMasCercanas[0], tresMasCercanas[1])
             elif dist < segundo and dist > primero:
@@ -102,14 +100,15 @@ class DataSetCampanasVerdes:
     def exportar_por_materiales(self, materiales:set[str]):
         '''
         Requiere: un conjunto de materiales 'Materiales'
-        Devuelve: Nada (habria que poner que crea un archivo?)
+        Devuelve: Nada
+        Modifica: EScribe un archivo CSV con nombre archivo_csv que contiene dos columnas, Direecion y Barrio, justamente incluyendo dicha informacion de las campanas en las cuales se puede tirar el/los material/es por parametro
         '''
-        f2 = open('archivo_csv', 'w')
-        f2.write('DIRECCION,BARRIO')
-        f2.write('\n')
-        for campana in self.campanas:
-            if materiales == campana.materiales:
-                f2.write(campana.direccion + ',' + campana.barrio + '\n')
+        f2 = open('archivo_csv', 'w') # abrimos un archivo con nombre 'archivo_csv' en modo write, por eso la 'w'
+        f2.write('DIRECCION,BARRIO') # le decimos que escriba como sus dos unicas columnas 'DIRECCION' y 'BARRIO'
+        f2.write('\n') # Hacemos un enter para que no se escriba en los titulos de las columnas
+        for campana in self.campanas: # Recorremos las campanas verdes
+            if materiales == campana.materiales: # Si el material o los materiales indicados por parametro se encuentran permitidos en la campana...
+                f2.write(campana.direccion + ',' + campana.barrio + '\n') # Escribimos en el archvio la direccion de la campana y el barrio de la misma, separados por una coma para asegurarnos de que esten en la columna que les corresponde, y por ultimo ponemos un enter para que la proxima vez que la proxima campana se escriba en la fila siguiente
         f2.close()
 
 ######################## FUNCION EXTRA PARA PODER TESTEAR ##########################################
