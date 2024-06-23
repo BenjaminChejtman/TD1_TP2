e11512d313ac99fbcfae6f1fabe1d1174b02b9b5
matuchiprut <166836106+matuchiprut@users.noreply.github.com>
2024-06-23 16:22:16 -0300

    Update campana_verde.py

--- tp2-v2(1)/tp2/templates/campana_verde.py
+++ tp2-v2(1)/tp2/templates/campana_verde.py
@@ -2,33 +2,31 @@ from haversine import haversine , Unit
 
 class CampanaVerde:
     def __init__(self, d:str, b:str, c:int, m:set[str], latylon:tuple[float, float]): #creamos los atributos de nuestra Campana Verde
-        ''' completar docstring '''
+        '''
+        Inicializa: una campana verde con dirección d, barrio b, comuna c, materiales m, latitud y longitud "latylon"
+        
+        '''
         self.direccion:str = d
         self.barrio:str = b
         self.comuna:int = c
         self.materiales:set[str] = m
-        #self.latitud:float = lat
-        #self.longitud:str = lon
         self.latitudylongitud:tuple[float, float] = latylon
 
-    def distancia(self, punto:tuple[float, float]) -> float:     
+    def distancia(self, punto:tuple[float, float]) -> float:       
         ''' 
         Requiere: una direccion valida con el eje X (o latitud) en el primer float de la tupla y con el eje Y (o longitud) en el segundo float de la tupla
-        Devuelve: La distancia entre el punto 1 y el punto 2, osea entre la ubicacion de la Campana Verde y el punto ingresado
+        Devuelve: La distancia entre el punto 1, siendo la campana verde, y el punto 2, el punto ingresado
         '''
-        return haversine(self.latitudylongitud, punto, unit=Unit.METERS) # devolvemos la distancia entre donde se encuentra la campana verde y un punto, es cual es una tupla del tipo (latitud, longitud), utilizando haversine
+        return haversine(self.latitudylongitud, punto, unit=Unit.METERS) # devolvemos la distancia entre la ubicación de la campana verde y un punto, el cual es una tupla del tipo (latitud, longitud), utilizando la función haversine
 
     def __repr__(self) -> str:
         '''
         Requiere: nada
-        Devuelve: Un mensaje de tipo STR que contiene la direccion, materiales y barrio de la campana verde C separados por @ y con <> al principio y al final respectivamente. Cabe mencionar que los materiales que se pueden depositar van a estar seprados por /.
+        Devuelve: Un mensaje de tipo STR que contiene la dirección, materiales y barrio de la campana verde C separados por @, con < y > al principio y al final respectivamente. Cabe mencionar que los materiales que se pueden depositar van a estar seprados por /.
         '''
         return ('<' + self.direccion + '@' + str('/'.join(self.materiales)) + '@' + self.barrio + '>')
 
-'''    
-    def __eq__(self, other) -> str:
-        return self.direccion == other.direccion and self.barrio == other.barrio and self.comuna == other.comuna and self.materiales == other.materiales and self.latitudylongitud == other.latitudylongitud
-'''
+
 
     
         
